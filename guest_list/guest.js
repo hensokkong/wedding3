0efa490aae45b16bf5786ef63e4c5ba8d5d1cd5e
hensokkong <sokkonghen05@gmail.com>
2026-01-12 14:14:00 +0700

    Add files via upload

--- guest_list/guest.js
+++ guest_list/guest.js
@@ -4,8 +4,8 @@
 //example link https://wedding-invitation-dkh9.vercel.app/?guest=002 (?guest=002) importance kut link 
 export const guestlistimg ={
         
-         "Chum_Chan_Phea_Run" : "img/list_URL/Chum_Chan_Phea_Run.jpg",
-
+     "Chum_Chan_Phea_Run" : "img/list_URL/Chum_Chan_Phea_Run.jpg",
+     "Li_Chan-Ri_Na" : "img/list_URL/Li_Chan-Ri_Na.jpg",
          
 
     };
