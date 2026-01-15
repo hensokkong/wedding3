0a30851a792f449bc4d75eb9a1c4cc0f46eb3398
hensokkong <sokkonghen05@gmail.com>
2026-01-15 17:17:42 +0700

    Add files via upload

--- guest_list/guest.js
+++ guest_list/guest.js
@@ -6,5 +6,6 @@ export const guestlistimg ={
         
      "Chum_Chan_Phea_Run" : "img/list_URL/Chum_Chan_Phea_Run.jpg",
      "Li_Chan-Ri_Na" : "img/list_URL/Li_Chan-Ri_Na.jpg",
-     "Say_Ra_Vy" : "img/list_URL/Say_Ra_Vy.jpg",  
+     "Say_Ra_Vy" : "img/list_URL/Say_Ra_Vy.jpg",
+     "Aim_Jea_Nai" : "img/list_URL/Aim_Jea_Nai.jpg",  
     };
